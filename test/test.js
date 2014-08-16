fc4294e4d3ea1c0b1dc4cb2882876f4b9c0992c9
Lars Eriksson <larsq.eriksson@gmail.com>
2014-08-16 08:50:22 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var expect = require("expect")
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      expect([1,2,3].indexOf(5)).toBe(-1)
+    })
+  })
+})
